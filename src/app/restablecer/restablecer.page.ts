7135b412c785ac9ced2aac91a4e63e7eb0fea9f7
Diego Cortes <diegocortes.pinilla@gmail.com>
2024-11-26 19:20:00 -0300

    ajustes Restablecer.ts

--- src/app/restablecer/restablecer.page.ts
+++ src/app/restablecer/restablecer.page.ts
@@ -7,24 +7,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./restablecer.page.scss'],
 })
 export class RestablecerPage implements OnInit {
-  username: string = ''; // Binding the username input
-  errorMessage: string = ''; // Error message placeholder
+  username: string = ''; 
+  errorMessage: string = ''; 
 
   constructor(private router: Router) {}
 
   ngOnInit() {}
 
-  // Function to handle the form submission
+
   onRecuperar() {
-    // Trim the username to remove leading/trailing spaces
+    
     const trimmedUsername = this.username.trim();
 
-    // Check if the username is empty or just spaces
+    
     if (!trimmedUsername) {
       this.errorMessage = 'El campo Usuario no puede estar vacío o contener solo espacios.';
     } else {
-      this.errorMessage = ''; // Clear the error message if input is valid
-      this.iralogin(); // Call the method to navigate to the login page
+      this.errorMessage = ''; 
+      this.iralogin(); 
     }
   }
 
