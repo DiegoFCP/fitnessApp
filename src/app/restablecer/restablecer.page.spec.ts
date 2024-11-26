09671b3fb0c1f9cf2edb4c4a8ef5a7a19a66e858
Diego Cortes <diegocortes.pinilla@gmail.com>
2024-11-26 19:19:32 -0300

    U.T. Restablecer

--- src/app/restablecer/restablecer.page.spec.ts
+++ src/app/restablecer/restablecer.page.spec.ts
@@ -1,11 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RestablecerPage } from './restablecer.page';
+import { FormsModule } from '@angular/forms';
+import { IonicModule,  } from '@ionic/angular';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 describe('RestablecerPage', () => {
   let component: RestablecerPage;
   let fixture: ComponentFixture<RestablecerPage>;
+  let mockRouter: any;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    mockRouter = {
+      navigate: jasmine.createSpy('navigate'),
+    };
+
+    await TestBed.configureTestingModule({
+      declarations: [RestablecerPage],
+      imports: [IonicModule.forRoot(), FormsModule, RouterTestingModule],
+      providers: [{ provide: Router, useValue: mockRouter }],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RestablecerPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -13,5 +28,25 @@ describe('RestablecerPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deberia mostrar error si el campo de usuario se encuentra vacio', () => {
+    component.username = '';
+    component.onRecuperar();
+    expect(component.errorMessage).toBe('El campo Usuario no puede estar vacío o contener solo espacios.');
+  });
+
+  it('deberia limpiar el mensaje de error y navegar a login si el usuario es valido', () => {
+    component.username = 'validUser';
+    component.onRecuperar();
+    expect(component.errorMessage).toBe('');
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/login']);
+  });
+
+  it('deberia mostrar error si el campo de usuario se llena con espacios', () => {
+    component.username = '   ';
+    component.onRecuperar();
+    expect(component.errorMessage).toBe('El campo Usuario no puede contener solo espacios.');
+    expect(mockRouter.navigate).not.toHaveBeenCalled();
   });
 });
