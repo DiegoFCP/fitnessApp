27bb046ecb5f0575bc16d907999ebd14814f2b7c
Diego Cortes <diegocortes.pinilla@gmail.com>
2024-11-26 16:40:38 -0300

    P.U. en componente 404

--- src/app/page-not-found/page-not-found.component.spec.ts
+++ src/app/page-not-found/page-not-found.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { PageNotFoundComponent } from './page-not-found.component';
+import { By } from '@angular/platform-browser';
+
 
 describe('PageNotFoundComponent', () => {
   let component: PageNotFoundComponent;
@@ -20,5 +22,18 @@ describe('PageNotFoundComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Validacion de mensaje de error correcto.
+  it('mostrar display con mensaje de error correcto', () => {
+    const errorMessage = fixture.debugElement.query(By.css('.error-message')).nativeElement;
+    expect(errorMessage.textContent).toContain('Lo sentimos, la página que buscas no existe.');
+  });
+  
+  // Validacion que exista título en DOM.
+  it('deberia tener titulo', () => {
+    const title = fixture.debugElement.query(By.css('h1')).nativeElement;
+    expect(title).toBeTruthy();
+    expect(title.textContent).toContain('Error 404'); // Ajustar según el texto real
   });
 });
