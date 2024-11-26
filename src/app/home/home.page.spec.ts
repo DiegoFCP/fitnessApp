f9c8c589ae47d2157629a958c26ddf6f9eb1a1ff
Diego Cortes <diegocortes.pinilla@gmail.com>
2024-11-26 19:03:35 -0300

    U.T. Home

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -4,11 +4,14 @@ import { IonicModule } from '@ionic/angular';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFireModule } from '@angular/fire/compat';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { PedometerService } from '../services/podometro.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
+//DECLARACION DE MOCKS
 
 const mockAngularFireAuth = {
   currentUser: Promise.resolve({ uid: 'test-uid' }),
@@ -22,8 +25,16 @@ const mockPedometerService = {
 
 const mockActivatedRoute = {
   snapshot: {
-    params: { id: '123' }, // Parametros de ruta
+    params: { id: '123' },
   },
+};
+
+const mockAngularFirestore = {
+  collection: (name: string) => ({
+    doc: (id: string) => ({
+      valueChanges: jasmine.createSpy('valueChanges').and.returnValue(of({ nombre: 'Test User' }))
+    })
+  })
 };
 
 describe('HomePage', () => {
@@ -35,22 +46,39 @@ describe('HomePage', () => {
       declarations: [HomePage],
       imports: [
         IonicModule.forRoot(),
-        HttpClientTestingModule, // Mock para HttpClient
-        AngularFireModule.initializeApp(environment.FIREBASE_CONFIG), // CONFIG de Firebase
+        HttpClientTestingModule,
+        AngularFireModule.initializeApp(environment.FIREBASE_CONFIG),
       ],
       providers: [
-        { provide: AngularFireAuth, useValue: mockAngularFireAuth }, // Mock de AngularFireAuth
-        { provide: PedometerService, useValue: mockPedometerService }, // Mock del podómetro
+        //MOCKS
+        { provide: AngularFireAuth, useValue: mockAngularFireAuth }, 
+        { provide: AngularFirestore, useValue: mockAngularFirestore }, 
+        { provide: PedometerService, useValue: mockPedometerService },
         { provide: ActivatedRoute, useValue: mockActivatedRoute },
       ],
     }).compileComponents();
-
+  
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+  
+
+  //PRUEBAS
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deberia traer nombre de Firestore y actualizar usuario', async () => {
+    await component.ngOnInit();
+    expect(component.nombreUsuario).toBe('Test User');
+  });
+
+  it('deberia llamar startTracking de PedometerService cuando inicializa', async () => {
+    await component.ngOnInit();
+    expect(mockPedometerService.startTracking).toHaveBeenCalled();
+  });
+  
+  
 });
