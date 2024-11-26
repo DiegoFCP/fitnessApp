64050d2459f6bfa9b6e9a1906cdade1ce28aeb37
Diego Cortes <diegocortes.pinilla@gmail.com>
2024-11-26 19:48:54 -0300

    P.U. Servicios

--- src/app/services/podometro.service.spec.ts
+++ src/app/services/podometro.service.spec.ts
@@ -1,29 +1,52 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { PedometerService } from './podometro.service';
-import { Pedometer } from '@ionic-native/pedometer/ngx';
+import { IPedometerData } from '@ionic-native/pedometer/ngx';
 
 describe('PedometerService', () => {
   let service: PedometerService;
 
-  const mockPedometer = {
-    isStepCountingAvailable: jasmine.createSpy('isStepCountingAvailable').and.resolveTo(true),
-    startPedometerUpdates: jasmine.createSpy('startPedometerUpdates').and.returnValue({
-      subscribe: jasmine.createSpy('subscribe'),
-    }),
-    stopPedometerUpdates: jasmine.createSpy('stopPedometerUpdates').and.callFake(() => Promise.resolve()),
+  const mockStepData: IPedometerData = {
+    numberOfSteps: 100,
+    distance: 200,
+    floorsAscended: 5,
+    floorsDescended: 3,
+  };
+
+  const mockPedometerService = {
+    stepData$: of(mockStepData),
+    startTracking: jasmine.createSpy('startTracking'),
+    stopTracking: jasmine.createSpy('stopTracking'),
   };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        PedometerService,
-        { provide: Pedometer, useValue: mockPedometer }, // Proveedor del mock de Pedometer
+        { provide: PedometerService, useValue: mockPedometerService },
       ],
     });
-    service = TestBed.inject(PedometerService); // Inyecta el servicio
+
+    service = TestBed.inject(PedometerService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy(); // Verifica que el servicio se crea correctamente
+    expect(service).toBeTruthy();
+  });
+
+  it('deberia subscribirse a stepData$ y recibir la info de los Pasos', (done) => {
+    service.stepData$.subscribe((data: any) => {
+      expect(data).toEqual(mockStepData);
+      done(); // Llamamos a done para finalizar la prueba asincrónica
+    });
+  });
+
+  it('deberia llamar a startTracking', () => {
+    service.startTracking();
+    expect(mockPedometerService.startTracking).toHaveBeenCalled();
+  });
+
+  it('deberia llamar a stopTracking', () => {
+    service.stopTracking();
+    expect(mockPedometerService.stopTracking).toHaveBeenCalled();
   });
 });
--- src/app/services/tips.service.spec.ts
+++ src/app/services/tips.service.spec.ts
@@ -1,19 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController  } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TipsService } from './tips.service';
-
 
 describe('TipsService', () => {
   let service: TipsService;
   let httpMock: HttpTestingController;
+  const mockUrl = 'https://cors-anywhere.herokuapp.com/https://frasesapi.vercel.app/v1/frases/aleatoria/?categoria=salud';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule], // Importa el módulo de pruebas de HttpClient
+      imports: [HttpClientTestingModule],
       providers: [TipsService],
     });
-    service = TestBed.inject(TipsService); // Inyecta el servicio
-    httpMock = TestBed.inject(HttpTestingController); // Inyecta HttpTestingController
+
+    service = TestBed.inject(TipsService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   afterEach(() => {
@@ -22,5 +23,22 @@ describe('TipsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+
+  it('deberia manejar errores al hacer consultas a la api', () => {
+    const mockError = { status: 404, statusText: 'Not Found' };
+
+    service.getHealthTip().subscribe(
+      () => fail('Expected an error, but received a response'),
+      (error) => {
+        expect(error.status).toBe(404);
+        expect(error.statusText).toBe('Not Found');
+      }
+    );
+
+    const req = httpMock.expectOne(mockUrl);
+    expect(req.request.method).toBe('GET');
+    req.flush(null, mockError); // Simula una respuesta de error
   });
 });
